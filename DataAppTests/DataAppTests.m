48d9d68d2fe6cb28687734899a093cc9ead924aa
lan1234tw <lan1234tw@gmail.com>
2014-03-13 08:37:43 +0800

    Initial Commit

---
+++ DataAppTests/DataAppTests.m
@@ -0,0 +1,34 @@
+//
+//  DataAppTests.m
+//  DataAppTests
+//
+//  Created by HsiuYi on 2014/3/13.
+//  Copyright (c) 2014年 HsiuYi. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DataAppTests : XCTestCase
+
+@end
+
+@implementation DataAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
